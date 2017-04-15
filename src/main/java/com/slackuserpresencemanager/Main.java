4722c6a9a6ea4d3aeaf160a2b3d5525ea9f9f799
Tharaka De Silva <tharaka.uo@gmail.com>
2017-04-15 03:54:00 -0500

    protype implementation of the application

---
+++ src/main/java/com/slackuserpresencemanager/Main.java
@@ -0,0 +1,30 @@
+package com.slackuserpresencemanager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Tharaka on 4/15/2017.
+ * Project: SlackUserPresenceManager
+ */
+public class Main {
+
+	private static final String PROPERTY_FILE_NAME = "build.properties";
+
+	public static void main (String[] args) {
+		Properties properties = new Properties();
+		InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(PROPERTY_FILE_NAME);
+		if (inputStream == null) {
+			throw new IllegalArgumentException("The config.properties file was not found!");
+		} else {
+			try {
+				properties.load(inputStream);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		WindowsSessionManager.setSlackToken(properties.getProperty("token"));
+		new WindowsSessionManager();
+	}
+}
